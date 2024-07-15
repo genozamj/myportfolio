34b5a4dde6b77ee8ddb8b095890bd29d9a66b3dc
genozamj <gen.marcjasper@gmail.com>
2024-07-16 01:11:25 +0800

    sample1

---
+++ module/Home/content.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react'
+
+const content = () => {
+  return (
+    <div className="flex flex-col items-center justify-center py-8">
+    <h1 className="text-3xl font-semibold">Let's work together.</h1>
+    <p className="text-md font-normal text-gray-500 text-center px-3">
+      Creating user experience and visual appealing design
+    </p>
+  </div>
+  )
+}
+
+export default content
---
+++ module/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Content from './content'
+const index = () => {
+  return (
+   
+   
+   <Content/>
+  )
+}
+
+export default index
